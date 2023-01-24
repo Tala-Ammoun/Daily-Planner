e89729927ac201d85b65d138ef00653a9e543732
Tala Ammoun <talaammoun@gmail.com>
2023-01-24 22:21:54 +0000

    color coded text areas in accordance with past, present, and future timing

--- starter/script.js
+++ starter/script.js
@@ -1,47 +1,78 @@
-$(function(){
+$(function () {
 
 
-setInterval(function(){
-    var today = moment(); //to use the variable today outside the function, we need to pass it as a parameter inside another function, then call that function outside the setInterval function.
+setInterval(function () {
+    var today = moment();
     document.querySelector("#currentDay").textContent = today.format("D MMM YYYY");
-})
+    })
 
-displayStoredText()
 
-Object.keys($(".hour")).forEach((element) =>  
-{ 
-    if (element <10){
-    $($(".hour")[element]).attr("data-index", element)
-    $($("textarea")[element]).attr("data-index", element)
-    $($(".saveBtn")[element]).attr("data-index", element)
-}}
+    function storedText() {
+        let values = []
+        keys = Object.keys(localStorage)
+        i = keys.length
+        while (i--) {
+            let storedText = JSON.parse(localStorage.getItem("textArea"))
+            values.push(localStorage.getItem(keys[i]))
+            $($("textarea")).text = storedText
+        }
+    }
+    storedText()
+
+    // let storedText= JSON.parse(localStorage.getItem("textArea"))
+    // console.log(storedText)
+    // $($("textarea")).text(storedText.textArea) //display text at the textAreaIndex where the text was entered
+
+    $(".saveBtn").click(function (event) {
+        let saveBtnIndex = $(event.target).data().index;
+        let textareaIndex = $(event.target).siblings().data().index;
+        if (saveBtnIndex = textareaIndex) {
+            let textArea = $("textarea").text(); //targets value entered at any textarea where both indexes are equal, but not at a specific textAreaIndex
+            localStorage.setItem("textArea", JSON.stringify({textArea: textArea[textareaIndex]}))
+        }
+        storedText()
+    })
+    
+
+let present = parseFloat(moment().format("HH"))
+
+Object.keys($(".hour")).forEach((element) => {
+    if (element < 10) {
+        $($(".hour")[element]).attr("data-index", element)
+        $($("textarea")[element]).attr("data-index", element)
+        $($(".saveBtn")[element]).attr("data-index", element)
+    }
+}
 )
 
-// let textArea = $("textarea").val()
+Object.keys($(".hour")).forEach((element) => {
+    if (element < 10) {
+        let hourNum = parseFloat($($(".hour")[element]).text(), 2)
+        let hourIndex = $($(".hour")[element]).attr("data-index")
+        let textareaIndex = $($("textarea")[element]).attr("data-index")
 
-$(".saveBtn").click(function(event){
-    event.preventDefault();
-    let saveBtnIndex = $(event.target).data().index
-    let textareaIndex = $(event.target).siblings().data().index
-    if (saveBtnIndex === textareaIndex){
-        let textArea = $("textarea").val()
-        localStorage.setItem("textArea", JSON.stringify({textArea}))
-    }
-})
+    if (hourNum === present && hourIndex === textareaIndex) {
+            $($("textarea")[textareaIndex]).css('background-color', '#73231a')}
 
-// let storedText = localStorage.getItem(textArea)
-// $("textarea").text(storedText)
+    else if (hourNum < present && hourIndex === textareaIndex) {
+            $($("textarea")[textareaIndex]).css('background-color', 'lightgrey')}
 
-function displayStoredText(){
-    let storedText = localStorage.getItem("textArea")
-    if (storedText === "")
-    {
-        return
-    }
-    else if (storedText === ""){
-        $("textarea").text(storedText)
-    }}
+    else if (hourNum > present && hourIndex === textareaIndex) {
+            $($("textarea")[textareaIndex]).css('background-color', 'olive')}
+                }
+            })
+
+        // function displayStoredText(){
+        //     let storedText = localStorage.getItem("textArea")
+        //     if (storedText === "")
+        //     {
+        //         return
+        //     }
+        //     else if (storedText === ""){
+        //         $("textarea").text(storedText)
+        //     }}
 
 
+        //$("textarea").sortable();
 
-})
+    })
